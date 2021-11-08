26a7eb54cc3969f4662429f4a679cdadfc642cf9
iourivolkov <volk.iouri@gmail.com>
2021-11-08 14:57:59 +0000

    fetch ISS flyovers for a given location - third api call

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const {fetchMyIP} = require('./iss');
 const {fetchCoordsByIP} = require('./iss');
+const {fetchISSFlyOverTimes} = require('./iss');
 
-// Fetch Ip address
+// Fetch Ip address - first api call
 // makes a single API request to retrieve user's IP address
 
 // fetchMyIP((error, ip) => {
@@ -13,14 +14,29 @@ const {fetchCoordsByIP} = require('./iss');
 //   console.log("It worked! Returned IP: ", ip);
 // });
 
-// fetch coordinates for a given IP
-fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+// fetch coordinates for a given IP - second api call
+// fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coordinates:' , coordinates);
+// });
+
+// fetch ISS flyovers for a given location - third api call
+
+// returns { latitude: 49.2643, longitude: -123.0961 }
+
+// fetch flyover times for given coordinates - third api call
+
+const coords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(coords, (error, passTimes) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned coordinates:' , coordinates);
+  console.log('It worked! Returned flyover times:' , passTimes);
 });
-
-// returns { latitude: 49.2643, longitude: -123.0961 }
