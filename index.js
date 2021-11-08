787789b8f8b7c5c575d3c99ebc68449537a8b4ab
iourivolkov <volk.iouri@gmail.com>
2021-11-08 13:36:37 +0000

    temporary test code

---
+++ index.js
@@ -0,0 +1,13 @@
+const {fetchMyIP} = require('./iss');
+// Fetch Ip address
+// makes a single API request to retrieve user's IP address
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log("It worked! Returned IP: ", ip);
+// });
+
