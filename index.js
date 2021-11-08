974ae3d32f959abf10a14c8df83836fbeb6086f7
iourivolkov <volk.iouri@gmail.com>
2021-11-08 14:12:45 +0000

    fetch coordinates for a given ip

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const {fetchMyIP} = require('./iss');
+const {fetchCoordsByIP} = require('./iss');
+
 // Fetch Ip address
 // makes a single API request to retrieve user's IP address
 
@@ -11,3 +13,14 @@ const {fetchMyIP} = require('./iss');
 //   console.log("It worked! Returned IP: ", ip);
 // });
 
+// fetch coordinates for a given IP
+fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
+});
+
+// returns { latitude: 49.2643, longitude: -123.0961 }
