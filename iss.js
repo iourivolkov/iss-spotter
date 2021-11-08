e7aa6b7dc46971e7a1380ab9b82342a4df1ecc60
iourivolkov <volk.iouri@gmail.com>
2021-11-08 14:59:42 +0000

    implement fetchISSFlyOverTimes

--- iss.js
+++ iss.js
@@ -44,8 +44,29 @@ const fetchCoordsByIP = function(ip, callback) {
 
 };
 
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=-${coords.longitude}`; request(url, (error, response, body) => {
+    // error handling if error is defined
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // error handling if error not defined but response code !== 200
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
+      return;
+    }
+
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
+
+  });
+
+};
 
 
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
 
